5da2a6decf959828b5449a36b228e87b9f53e121
pilquito <pilquito@users.noreply.github.com>
2025-11-13 21:46:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from app import app
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# Este archivo es usado por Gunicorn
+# NO ejecutar con python main.py directamente
+# Usar: gunicorn main:app
